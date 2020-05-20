f60ae77ce69f2bbd51053e422ca259a2ba2acb47
toufik <les.char2th@gmail.com>
2020-05-20 23:12:17 +0100

    3rd commit

--- hello.py
+++ hello.py
@@ -1,3 +1,15 @@
-print('toufik')
-print ('d')
-print("sekhraoui")
+#introduction 
+fname = 'toufik'
+lname = 'sekhraoui'
+full_name = 'toufik sekhraoui'
+age = 2020 - 1996 
+user_name = 'ic_htoufik'
+print(fname.upper() + " " + lname.upper())
+print(full_name.replace('toufik','tawfeek'))
+print(age)
+print(user_name)
+
+b = 'thnx 2 fadi' * 1000
+print (b)
+
+
---
+++ num.py
@@ -0,0 +1,9 @@
+print(1 + 1)
+print(10 - 5)
+age = 2020 - int('1996')
+print(2020 - int('1996'))
+print(age)
+kg = 62
+g = 0.4
+wight = kg + g
+print(wight)
