19baac2ded5e6fd359b726ed80416b60606524c0
toufik <les.char2th@gmail.com>
2020-05-22 01:54:32 +0100

    last

---
+++ operators.py
@@ -0,0 +1,38 @@
+# byear = birth year / cyear = current year 
+
+byear = 1996
+cyear = 2020
+print(byear < cyear)
+print(byear > cyear)
+print(byear == cyear)
+
+print(byear != cyear)
+print(byear >= cyear)
+print(byear <= cyear)
+
+#logical operators 
+print(True and False)
+print(False and True)
+print(True and True)
+print(byear and cyear)
+print(cyear and byear)
+print(cyear and cyear)
+print(byear and byear)
+
+#other operators
+a,b = 2,8
+
+print(a == a and b == b)
+print(a != b and b == a)
+print(a == a or b == b)
+print(a == a or b == a)
+print(a < b or a > b)
+
+# not
+print(not (a != 5))
+print(not (a == b))
+
+
+
+
+
