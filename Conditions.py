8602aadbdeb9680bc5b0f9ce8491ccfbe0dc0a42
toufik <les.char2th@gmail.com>
2020-05-26 00:11:35 +0100

    con

--- Conditions.py
+++ Conditions.py
@@ -1,6 +1,6 @@
 username = input('please insert your username or usereemail: ')
 password = input('please insert youre password: ')
-if username == 'toufik' and password == '123' :
+if username == 'toufik' and password == '1234' :
     print('user logged in successfully')
 else:
     print('error')
