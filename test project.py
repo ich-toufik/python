bad2db35d70aeed9506742bcb3c7335f16e23c7b
toufik <les.char2th@gmail.com>
2020-06-01 15:46:37 +0100

    b

---
+++ test project.py
@@ -0,0 +1,11 @@
+#introduction 
+fname = 'toufik'
+lname = 'sekhraoui'
+full_name = 'toufik sekhraoui'
+age = 24 
+user_name = 'ic_htoufik'
+print(fname.upper() + " " + lname.upper())
+print(full_name.replace('toufik','tawfeek'))
+print(age)
+print(user_name)
+
